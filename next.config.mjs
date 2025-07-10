a551367a0c63d119c42750886522a9a1dca291c4
Martin Lynn <54124417+martin473@users.noreply.github.com>
2025-07-10 14:21:42 -0700

    changed lint config on build to off

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 const withMDX = createMDX({
